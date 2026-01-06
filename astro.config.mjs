3fbd5e342108deb4251e09cc928cf0cc9a510f00
Shunsuke NAKATSUKA <jacketgardensio@gmail.com>
2026-01-06 21:47:33 +0900

    feat: Add sitemap integration to Astro configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://salty-vanilla.vercel.app/',
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
